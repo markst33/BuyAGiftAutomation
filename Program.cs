a788fa923b9424c238dc6d7dcda484377e375c34
Mark <mstewart@paddypower.com>
2019-12-13 13:55:59 +0000

    Additional Tests

--- Program.cs
+++ Program.cs
@@ -30,10 +30,10 @@ namespace BuyAGiftAutomation
 
             
             //Compare Expected Price with Actual Price
-            string Price = "£28";
+            string Price = "£29";
             var ProductPrice = driver.FindElement(By.ClassName("search-product-price"));
             AssertLibrary.Assert.IsEqual(ProductPrice.Text, Price);
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
 
 
             //Click on 1st Product in listing
@@ -42,8 +42,9 @@ namespace BuyAGiftAutomation
 
 
             //Wait for product page to load and click Buy
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
             IWebElement BuyNowBtn = driver.FindElement(By.CssSelector("#product-form > div > button"));
+            Thread.Sleep(3000);
             BuyNowBtn.Click();
 
 
@@ -69,6 +70,39 @@ namespace BuyAGiftAutomation
             //Click Pay Securely
             IWebElement PaySecurely = driver.FindElement(By.CssSelector("#basket_payment_options > a:nth-child(1) > button"));
             PaySecurely.Click();
+
+
+            //Enter Email address and continue as guest
+            IWebElement EnterEmail = driver.FindElement(By.CssSelector("#account_email_field"));
+            EnterEmail.SendKeys("mark@mail.com");
+            IWebElement ContinueAsGuest = driver.FindElement(By.CssSelector("#checkout_capture > div.step1_btns.col-lg-offset-4 > button.btn.login_guest.chk_btns"));
+            ContinueAsGuest.Click();
+
+
+            //Enter customer details
+            IWebElement Title = driver.FindElement(By.Name("Salutation"));
+            Title.Click();
+            IWebElement TitleOptions = driver.FindElement(By.CssSelector("#titlefield > option:nth-child(2)"));
+            TitleOptions.Click();
+            IWebElement FName = driver.FindElement(By.CssSelector("#firstnamefield"));
+            FName.SendKeys("TestFirstName");
+            IWebElement LName = driver.FindElement(By.CssSelector("#lastnamefield"));
+            LName.SendKeys("TestLastName");
+            IWebElement ContinueBtn = driver.FindElement(By.CssSelector("#guest_personal_details > div.form-group.external_links > button"));
+            ContinueBtn.Click();
+
+
+            //Enter Billing and Payment details
+            IWebElement PostCode = driver.FindElement(By.CssSelector("#billingPostcode"));
+            PostCode.SendKeys("E1 6AN");
+            IWebElement SearchBtn = driver.FindElement(By.CssSelector("##postCodeSearchBilling > div > div.form-group.postcodesearch > div.continue_area_buttons.col-sm-2 > button"));
+            SearchBtn.Click();
+            IWebElement ChooseAddress = driver.FindElement(By.CssSelector("#new_card_selectaddress > ul > li.row.newcard_newaddress.subpanel-inner > div > div > div > div > div.form-group.postcodesearchlist > div.col-xs-12.col-sm-8.nopadding.highlighted-field-onload.highlighted-field-fadedaway > select"));
+            ChooseAddress.Click();
+
+
+
+
 
 
 
